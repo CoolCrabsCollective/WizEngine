f1e04d748f3220ccd9b4640c5379797f13fb9dda
Alexander Winter <a.w1nter@hotmail.com>
2022-03-28 12:20:53 -0400

    Done mapping functionality, tested with unit test

--- include/WIZ/input/MappingDatabase.h
+++ include/WIZ/input/MappingDatabase.h
@@ -15,10 +15,21 @@ namespace wiz {
 
 class wiz::MappingDatabase {
 	std::map<std::string, Mapping> map;
+	static MappingDatabase instance;
 public:
-	void loadFromCSV(const std::string& csvDatabase);
+	void loadFromCSVFile(const std::string& csvDbFile);
+
+	void loadFromCSV(const std::string& csvDbContent);
+
+	void addMapping(const std::string& controllerName, Mapping mapping);
+
+	void clearMappings();
 
 	const Mapping& getMapping(const std::string& controllerName) const;
+
+	bool hasMapping(const std::string& controllerName) const;
+
+	static const MappingDatabase& getInstance();
 };
 
 
