8379b0c56bb8c9b05f0ac4b8bc0f7584776456f2
Alexander Winter <a.w1nter@hotmail.com>
2022-04-01 19:58:27 -0400

    Added installation of box2d and sfml

--- switch-install.sh
+++ switch-install.sh
@@ -3,9 +3,14 @@
 set -eu
 cmake -B build-switch -S . -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/DevkitA64Libnx.cmake
 cd build-switch
+cd _deps/box2d-src
+./switch-install.sh
+cd ../..
+cd _deps/sfml-src
+./switch-install.sh
+cd ../..
 make
 cd ..
 
 sudo cp bin/switch/libWizEngine.a $DEVKITPRO/portlibs/switch/lib/
 sudo cp -r include/* $DEVKITPRO/portlibs/switch/include/
-#sudo cp -r build-switch/_deps/sfml-src/include/* $DEVKITPRO/portlibs/switch/include/
